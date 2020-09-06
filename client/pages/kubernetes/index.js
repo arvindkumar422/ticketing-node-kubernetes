6cef5de8398b2bb17a100caac5e9e4a8dd00d1d2
Arvind Kumar <arvindkumar422@gmail.com>
2020-09-05 21:32:55 -0400

    added file for kube

---
+++ client/pages/kubernetes/index.js
@@ -0,0 +1,43 @@
+import depls from '../../assets/depl.json';
+
+const Kube = () => {
+  return (
+    <div>
+      <h3>
+        Deployments
+      </h3>
+      <hr />
+      <table class="order-table table table-secondary">
+        <thead>
+          <tr>
+            <th>
+              Name
+            </th>
+            <th>
+              Created
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {depls.depls.forEach((element) => {
+            return (
+              <tr key={element.id}>
+                <td>{element.name}</td>
+                <td>{element.date}</td>
+              </tr>
+            );
+          })}
+
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+Kube.getInitialProps = async (context, client) => {
+  const { data } = await client.get('/api/orders');
+
+  return { orders: data };
+};
+
+export default Kube;
