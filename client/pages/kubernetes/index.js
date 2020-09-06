db0a90889fc6c00464fbdd524b10c140463da730
Arvind Kumar <arvindkumar422@gmail.com>
2020-09-05 22:27:26 -0400

    added filez

--- client/pages/kubernetes/index.js
+++ client/pages/kubernetes/index.js
@@ -1,32 +1,78 @@
 import depls from '../../assets/depl.json';
+import services from '../../assets/services.json';
 
 const Kube = () => {
+
+  const [selection, setSelection] = useState('');
+
+
+
   return (
     <div>
-      <h3>
-        Deployments
-      </h3>
+      <ul class="nav nav-tabs">
+        <li class="nav-item">
+          <a class="nav-link" value="aaa" onChange={(e) => setSelection(e.target.value)}>Aaaaaa</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" value="aaa" onChange={(e) => setSelection(e.target.value)}>bbbbb</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" value="sdff" onChange={(e) => setSelection(e.target.value)}>ccccc</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" value="dfs" href="#" onChange={(e) => setSelection(e.target.value)}>ddddd</a>
+        </li>
+      </ul>
       <hr />
-      <table class="order-table table table-secondary">
+      <table class="order-table table">
         <thead>
           <tr>
             <th>
               Name
             </th>
             <th>
+              Namespace
+            </th>
+            <th>
+              Labels
+            </th>
+            <th>
+              Pods
+            </th>
+            <th>
               Created
+            </th>
+            <th>
+              Images
             </th>
           </tr>
         </thead>
         <tbody>
-          {depls.depls.map((item) => {
+          { selection === 'dfs' ? depls.depls.map((item) => {
             return (
               <tr key={item.name}>
                 <td>{item.name}</td>
-                <td>{item.date}</td>
+                <td>{item.namespace}</td>
+                <td>{item.labels}</td>
+                <td>{item.pods}</td>
+                <td>{(new Date().getHours() - new Date(item.created).getHours()) / 24} days ago</td>
+                <td>{item.images}</td>
               </tr>
             );
-          })}
+          })
+        : services.services.map((item) => {
+          return (
+            <tr key={item.name}>
+              <td>{item.name}</td>
+              <td>{item.namespace}</td>
+              <td>{item.labels}</td>
+              <td>{item.clusterip}</td>
+              <td>{(new Date().getHours() - new Date(item.created).getHours()) / 24} days ago</td>
+              <td>{item.endpoints}</td>
+            </tr>
+          );
+        })
+        }
 
         </tbody>
       </table>
